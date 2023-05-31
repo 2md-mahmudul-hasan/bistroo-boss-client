469b904eb4c7ab20410e87d3b9185bab1afd3391
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-05-31 16:48:56 +0600

    add scetion_title_components

---
+++ src/components/section_title/SectionTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SectionTitle({subheading, heading}) {
+  return (
+    <div className='my-4 text-center w-1/4 mx-auto'> 
+      <p className='text-warning'>{subheading}</p>
+      <h2 className='font-3xl border-y-4 py-4'>{heading}</h2>
+    </div>
+  )
+}
--- src/pages/home/order_online/Order.jsx
+++ src/pages/home/order_online/Order.jsx
@@ -12,11 +12,14 @@ import slider2 from '../../../assets/home/slide2.jpg'
 import slider3 from '../../../assets/home/slide3.jpg'
 import slider4 from '../../../assets/home/slide4.jpg'
 import slider5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/section_title/SectionTitle';
 
 
 const Order = () => {
   return (
-    <Swiper
+    <>
+       <SectionTitle heading={"Order Now "} subheading={"-- From 11 Am to 12 To Pm--"}></SectionTitle>
+       <Swiper
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={40}
@@ -25,10 +28,12 @@ const Order = () => {
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
+      className='mb-8'
     >
+   
       <SwiperSlide>
          <img src={slider1} alt='image' />
-         <h3 className='text-center font-3xl -mt-12 '>Salad</h3>
+         <h3 className='text-center font-3xl -mt-12 '>Salad...........</h3>
          </SwiperSlide>
       <SwiperSlide> 
         <img src={slider2} alt='image' />
@@ -71,6 +76,8 @@ const Order = () => {
 
 
     </Swiper>
+    </>
+    
   );
 };
 
